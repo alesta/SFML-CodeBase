b0642188632a3399aa1e22c57fce3d46a188e554
Alexander Stangl <alesta@bergen.org>
2013-12-15 20:38:33 -0500

    Ludum Dare

---
+++ Platformer/include/Enemy.h
@@ -0,0 +1,18 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include <SFML/Graphics.hpp>
+#include "Mob.h"
+
+
+class Enemy : public Mob {
+public:
+        Enemy();
+protected:
+
+private:
+    sf::Sprite e_sprite;
+    sf::Texture e_tex;
+};
+
+#endif // ENEMY_H
